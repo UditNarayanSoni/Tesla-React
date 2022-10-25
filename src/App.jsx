f9f2fbaf4eecc31d1da4eef076cce2844cd6d63e
Udit <uditseth188@gmail.com>
2022-10-25 16:54:50 +0530

    Tesla HomePage

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Nav from './components/Nav'
+import SingleCarOrHome from './components/SingleCarOrHome'
+
+function App() {
+  return (
+    <>
+      <Nav />
+      <SingleCarOrHome />
+    </>
+  )
+}
+
+export default App;
